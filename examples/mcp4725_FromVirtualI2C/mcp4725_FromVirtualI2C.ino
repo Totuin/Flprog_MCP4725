41d63e436e5a9374f516b5e04dbcf9aebac5b0d8
Totuin <117037443+Totuin@users.noreply.github.com>
2023-02-04 17:02:44 +0400

    Update mcp4725_FromVirtualI2C.ino

--- examples/mcp4725_FromVirtualI2C/mcp4725_FromVirtualI2C.ino
+++ examples/mcp4725_FromVirtualI2C/mcp4725_FromVirtualI2C.ino
@@ -5,11 +5,13 @@ FLProgTCA9548A commutator(&wireDevice);
 FLProgVirtualI2C virtualWire(&commutator, 2);
 FLProgMCP4725 dac(&virtualWire);
 
-void setup(void) {
+void setup(void)
+{
   wireDevice.begin();
 }
 
-void loop(void) {
+void loop(void)
+{
   uint32_t counter;
   for (counter = 0; counter < 4095; counter++)
   {
@@ -21,5 +23,4 @@ void loop(void) {
     dac.setVoltage(counter, false);
     delay(2);
   }
-
 }
